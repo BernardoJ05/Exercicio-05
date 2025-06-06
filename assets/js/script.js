d96bab3831ebdb0cf6e74d8706f739c36b4c466b
BernardoJ05 <kukorocha05@gmail.com>
2025-06-06 19:42:03 -0300

    06/06/2025 19:42

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
-const display = document.querySelector('.resultado');
+//temas
+document.body.classList.add('theme-1');
+const themeToggle = document.querySelector('.theme-toggle');
+const body = document.body;
+
+let currentTheme=1;
+
+themeToggle.addEventListener('click', () => {
+    currentTheme++;
+    if (currentTheme > 3) currentTheme = 1;
+    body.className = `theme-${currentTheme}`;
+});
+
+//funcional
+const display = document.querySelector('.result');
 const number = document.querySelectorAll('.number');
 const operator = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equals');
-const reset = document.querySelector('.reset');
-const del = document.querySelector('.del');
+const reset = document.querySelector('.span-2');
+const del = document.querySelector('.action');
+
 
 number.forEach(button => {
     button.addEventListener('click', ()=> {
@@ -14,5 +29,31 @@ number.forEach(button => {
             display.textContent += value;
         }
     });
-
 });
+
+
+operator.forEach(button => {
+    button.addEventListener('click', ()=> {
+        const value = button.textContent;
+        if (display.textContent += value) {
+        }
+    });
+});
+
+equals.addEventListener('click', ()=> {
+    try {
+        const result = eval(display.textContent.replace('x', '*'));
+        display.textContent = result;
+    } catch {
+        display.textContent = "Erro";
+    }
+});
+
+reset.addEventListener('click', () => {
+    display.textContent = '0';
+});
+
+del.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0, -1) || '0';
+});
+
